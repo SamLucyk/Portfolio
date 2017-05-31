f5163d2b778230045ddeab9fcc8de5d3d00ce41b
Sam <Sam@SLucyk.local>
2017-05-31 17:56:48 -0400

    Changed the port back to 80

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ io.on('connection', function(socket){
 require('./app/routes.js')(app, io); // load our routes and pass in our app and fully configured passport
 
 
-server.listen(8080, function(){
+server.listen(80, function(){
   console.log('listening on *:80');
 });
